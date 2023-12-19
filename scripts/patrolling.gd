8bb5b3edd031a1e9a2b64df01b96cbc1c4df30e2
aaaaaaaaa <mill9134@umn.edu>
2023-12-19 00:19:31 -0600

    so much

---
+++ scripts/patrolling.gd
@@ -0,0 +1,20 @@
+extends RigidBody3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var movement_vector = Vector3(0, 0, 5 * delta)
+	
+	if self.position.x > 21 || self.position.x < -21: 
+		self.rotate(Vector3.UP, deg_to_rad(180))
+		if self.position.x > 21:
+			self.position.x = 21
+		else:
+			self.position.x = -21
+	
+	self.position += movement_vector.rotated(Vector3.UP, self.rotation.y)
+	
