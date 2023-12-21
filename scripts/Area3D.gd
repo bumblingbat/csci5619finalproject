3fb64e9c14cdb39d40a632ebc97a9d04ee928da4
aaaaaaaaa <mill9134@umn.edu>
2023-12-21 11:51:21 -0600

    making world look bigger

--- scripts/Area3D.gd
+++ scripts/Area3D.gd
@@ -3,5 +3,6 @@ extends Area3D
 signal reset
 
 func _on_area_entered(area):
-	if area == $"../../XRUser/XROrigin3D/PlayerArea":
+	#if area == $"../../XRUser/XROrigin3D/PlayerArea":
+	if area == %PlayerArea:
 		reset.emit()
